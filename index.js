1406ce5d901dc271646fbf332563cc99e355f099
mahedi066 <imam25349@gmail.com>
2024-06-05 02:09:33 +0600

    allmeal api added

--- index.js
+++ index.js
@@ -31,8 +31,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     
+    // all collection 
 
     const mealsCollection=client.db('hostelDB').collection('meals')
+    const reviewCollection=client.db('hostelDB').collection('reviews')
+
+
+
+    // get all meals 
     app.get('/meals',async(req,res)=>{
         const result=await mealsCollection.find().toArray()
         res.send(result)
@@ -46,7 +52,11 @@ async function run() {
         res.send(result)
     })
 
-
+    // get reviews/ what our client says
+    app.get('/reviews',async(req,res)=>{
+      const result = await reviewCollection.find().toArray()
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
