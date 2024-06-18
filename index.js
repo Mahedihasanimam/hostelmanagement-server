f2e54b030b1d775e99aee2683990437839df9749
mahedi066 <imam25349@gmail.com>
2024-06-18 20:20:07 +0600

    patch method added for user role

--- index.js
+++ index.js
@@ -100,6 +100,24 @@ async function run() {
       const result=await usersCollection.insertOne(user)
       res.send(result)
     })
+
+
+
+    ///ADMIN RELATED API-----------------------------//
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id =req.params.id
+      const filter ={_id:new ObjectId(id)}
+      const updateDoc={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result=await usersCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
+
+
+
     // GET SECTION
     //------------------------------------------------------------//
 
@@ -212,7 +230,7 @@ async function run() {
     app.get('/users',async (req,res)=>{
       const result=await usersCollection.find().toArray()
       res.send(result)
-      
+
     }) 
     
     console.log(
